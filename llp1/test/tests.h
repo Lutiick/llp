163761fb4df5720b58290d93b4a79c499b543a05
lutiick <lutiick@gmail.com>
2024-04-02 14:32:47 +0300

    lab1 done

---
+++ llp1/test/tests.h
@@ -0,0 +1,15 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <time.h>
+#include "generator.h"
+#include "../include/query.h"
+
+int run_tests(char* db_file_path, char* tests_res_file_path);
+
+#endif
